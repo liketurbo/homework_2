7159bb990509eb474c800868124b81cf66e8a2b6
Ramzan Chasygov <theliketurbo@gmail.com>
2018-03-17 03:01:17 +0300

    in-out crash state

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,10 @@
-import { TimelineLite } from 'gsap';
-import { addEffect, runEffect, getRandomInt } from './functions';
+import { addEffect, runEffect, resizeElem } from './functions';
 
 // import './side-element';
-// import './listeners';
+import './listeners';
 import '../styles/main.sass';
+
+resizeElem('#canvas', '.container');
 
 const URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 const video = document.createElement('video');
@@ -16,8 +17,8 @@ navigator.mediaDevices.getUserMedia({
 const canvas = document.querySelector('#canvas');
 
 const ctx = canvas.getContext('2d');
-addEffect(canvas, 'displacement');
-setInterval(() => runEffect(), 1000);
+// addEffect(canvas, 'displacement');
+// setInterval(() => runEffect(), 1000);
 
 const loop = () => {
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
